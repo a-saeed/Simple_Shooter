91b5ca04539902871b6e3fe15eb2ea318689303a
Blue_Gene <khalifaabdallah101@gmail.com>
2022-10-22 22:47:15 -0700

    Make the enemy follow if I'm in his line of sight
    Initial setup for the Behavior Tree  and its Blackboard

--- EnemyAIController.cpp
+++ EnemyAIController.cpp
@@ -3,6 +3,7 @@
 
 #include "EnemyAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AEnemyAIController::BeginPlay()
 {
@@ -10,11 +11,32 @@ void AEnemyAIController::BeginPlay()
 	if (behaviorAI)
 	{
 		RunBehaviorTree(behaviorAI);
+		
+		//get pawns for the player, and the enemy (any character controlled by this ai controller)
+		APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+		APawn* enemyPawn = GetPawn();
+
+		//setting the player/enemy character location as a blackboard key
+		
+		GetBlackboardComponent()->SetValueAsVector(TEXT("startLocation"), enemyPawn->GetActorLocation());
 	}
 }
 
 void AEnemyAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+	//get the playable character
+	APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	//if the player pawn is in the enemy ai line of sight
+	if (LineOfSightTo(playerPawn))
+	{
+		//update the player location and player last known location
+		GetBlackboardComponent()->SetValueAsVector(TEXT("playerLocation"), playerPawn->GetActorLocation());
+		GetBlackboardComponent()->SetValueAsVector(TEXT("playerLastKnownLocation"), playerPawn->GetActorLocation());
+	}
+	else
+	{
+		GetBlackboardComponent()->ClearValue(TEXT("playerLocation"));
+	}
 }
