2e2f612bafbc0b09c54f604a0f03553ea5c5d5a7
Blue_Gene <khalifaabdallah101@gmail.com>
2022-10-23 18:12:17 -0700

    remove logic AIController class now that the services are responsible

--- EnemyAIController.cpp
+++ EnemyAIController.cpp
@@ -12,12 +12,9 @@ void AEnemyAIController::BeginPlay()
 	{
 		RunBehaviorTree(behaviorAI);
 		
-		//get pawns for the player, and the enemy (any character controlled by this ai controller)
-		APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 		APawn* enemyPawn = GetPawn();
 
-		//setting the player/enemy character location as a blackboard key
-		
+		//setting the enemy character location as a blackboard key
 		GetBlackboardComponent()->SetValueAsVector(TEXT("startLocation"), enemyPawn->GetActorLocation());
 	}
 }
@@ -25,18 +22,4 @@ void AEnemyAIController::BeginPlay()
 void AEnemyAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	//get the playable character
-	APawn* playerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
-	//if the player pawn is in the enemy ai line of sight
-	if (LineOfSightTo(playerPawn))
-	{
-		//update the player location and player last known location
-		GetBlackboardComponent()->SetValueAsVector(TEXT("playerLocation"), playerPawn->GetActorLocation());
-		GetBlackboardComponent()->SetValueAsVector(TEXT("playerLastKnownLocation"), playerPawn->GetActorLocation());
-	}
-	else
-	{
-		GetBlackboardComponent()->ClearValue(TEXT("playerLocation"));
-	}
 }
